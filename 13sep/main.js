e655eeabaea9fadaa320ad09095f49911b8fbab8
Abigail Rojas <yesseniadayan23@gmail.com>
2022-09-13 14:36:41 -0300

    conocimos if , else y else if

---
+++ 13sep/main.js
@@ -0,0 +1,27 @@
+// Declarar 4 variables y 1 constante
+let date ="13 septiembre"
+let level = "segundo"
+const car ="red"
+let number1= 10
+let number2= 30
+
+// Tipo de Datos
+let flag = true  // Boolean , dicotomicas
+let text = "El texto va entre comilla" // String texto
+let number = 1 // Number numeros enteros o decimales 
+
+//Arrays, cada elemento tiene un indice y parten desde 0
+let arr = (1, 2, 3, 4) // Arrays (arreglos )
+let arr2 =("texto", (1,2,3,4) ,5 , true) //Arrays
+
+arr [2] //Acceder al valor 4
+arr [2] = 5 //Asignando un nuevo valor
+
+// variableName(index)=> acceder al valor en un arreglo
+
+let nothing= null  //Tipo de dato para representar nada no tiene niun valor
+// Null representa un valor "vacio" o "nulo"
+
+let nodef = undefined
+let width // Sin un valor asignado , esta variable es "undefined"
+//undefined suele no usarse , ya que javascript lo hace 
