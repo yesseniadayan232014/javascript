2578ba58592be4ea0da862d27b151ffafe6ba57f
Abigail Rojas <yesseniadayan23@gmail.com>
2022-09-27 14:50:19 -0300

    Subiendo clases

---
+++ 12sep/main.js
@@ -0,0 +1,18 @@
+
+let nombre // declarar
+nombre = "alejandro" // asignar un valor
+
+/* const permite declarar variables constantes, 
+no se puede cambiar el valor en la ejecucion del programa */
+const pi = 3.1416 // declarando y asignando un valor
+
+// let permite declarar una variable y luego cambiar su valor
+let apellido
+
+nombre = "esteban"
+apellido = "nuñez" // String
+
+const numero1 = 1 // Number
+const numero2 = 15
+
+console.log(numero1 + numero2)
